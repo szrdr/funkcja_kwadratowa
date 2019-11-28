d42e3c737056521540f25a867a68db49f5950bc5
szrdr <nowaprzygoda1!>
2019-11-28 17:32:28 +0100

    Obliczanie delty dodatniej

--- app.py
+++ app.py
@@ -6,3 +6,17 @@ delta = 0
 miejsce_zerowe1 = 0
 miejsce_zerowe2 = 0
 
+# Obliczanie delty
+
+delta = pow(parametr_b, 2) - 4 * parametr_a * parametr_c
+
+# Sprawdzanie czy delta jest dodatnia lub ujemna
+
+if delta >= 0:
+    miejsce_zerowe1 = (-parametr_b + delta)/(2*parametr_a)
+    miejsce_zerowe2 = (-parametr_b - delta)/(2*parametr_a)
+    print(f"Miejsce zerowe 1: {miejsce_zerowe1}")
+    print(f"Miejsce zerowe 2: {miejsce_zerowe2}")
+else:
+    print('delta ujemna')
+
