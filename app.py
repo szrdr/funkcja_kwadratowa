0c63053ef6e9c7a6ea79b674bc92142b94c4eded
szrdr <Nowaprzygoda1!>
2019-11-28 18:54:24 +0100

    Obliczanie delty ujemnej

--- app.py
+++ app.py
@@ -1,22 +1,28 @@
-# Dodanie potrzebnych zmiennych
-parametr_a = int(input("Podaj parametr a: "))
-parametr_b = int(input("Podaj parametr b: "))
-parametr_c = int(input("Podaj parametr c: "))
-delta = 0
-miejsce_zerowe1 = 0
-miejsce_zerowe2 = 0
+def oblicz_miejsce_zerowe(a,b,c):
+    import cmath
+    import math
 
-# Obliczanie delty
+    # Dodanie potrzebnych zmiennych
+    parametr_a = a
+    parametr_b = b
+    parametr_c = c
+    delta = 0
+    miejsce_zerowe1 = 0
+    miejsce_zerowe2 = 0
 
-delta = pow(parametr_b, 2) - 4 * parametr_a * parametr_c
+    # Obliczanie delty
+    delta = pow(parametr_b, 2) - 4 * parametr_a * parametr_c
 
-# Sprawdzanie czy delta jest dodatnia lub ujemna
+    if delta >= 0:  # Sprawdzanie czy delta jest dodatnia
+        miejsce_zerowe1 = (-parametr_b + math.sqrt(delta)) / (2 * parametr_a)
+        miejsce_zerowe2 = (-parametr_b - math.sqrt(delta)) / (2 * parametr_a)
+        print(f"Miejsce zerowe 1: {miejsce_zerowe1}")
+        print(f"Miejsce zerowe 2: {miejsce_zerowe2}")
+    else:  # Sprawdzanie czy delta jest ujemna
+        delta_zespolona = complex(0, math.sqrt(-delta))
+        miejsce_zerowe1 = (complex(-parametr_b, 0) + delta_zespolona) / (complex(2 * parametr_a, 0))
+        miejsce_zerowe2 = (complex(-parametr_b, 0) - delta_zespolona) / (complex(2 * parametr_a, 0))
+        print(f"Miejsce zerowe 1: {miejsce_zerowe1}")
+        print(f"Miejsce zerowe 2: {miejsce_zerowe2}")
 
-if delta >= 0:
-    miejsce_zerowe1 = (-parametr_b + delta)/(2*parametr_a)
-    miejsce_zerowe2 = (-parametr_b - delta)/(2*parametr_a)
-    print(f"Miejsce zerowe 1: {miejsce_zerowe1}")
-    print(f"Miejsce zerowe 2: {miejsce_zerowe2}")
-else:
-    print('delta ujemna')
-
+oblicz_miejsce_zerowe(1,2,3)
