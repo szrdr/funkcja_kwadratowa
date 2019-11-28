eaa27d00a07261ba5871a85411950554d264a795
szrdr <Nowaprzygoda1!>
2019-11-28 17:14:06 +0100

    Add var

---
+++ app.py
@@ -0,0 +1,8 @@
+# Dodanie potrzebnych zmiennych
+parametr_a = int(input("Podaj parametr a: "))
+parametr_b = int(input("Podaj parametr b: "))
+parametr_c = int(input("Podaj parametr c: "))
+delta = 0
+miejsce_zerowe1 = 0
+miejsce_zerowe2 = 0
+
